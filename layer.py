3ad6f6f5002b43b7f13dff42a269ac1899f59561
Alessandro Bufalino <Alessandro2.Bufalino@live.uwe.ac.uk>
2021-05-04 11:12:35 +0100

    Quick bug fix for the passage bool

--- layer.py
+++ layer.py
@@ -5,12 +5,12 @@ class Layer:
         self.name = ""
         self.tiles = []
         self.layer_cost = 0
-        self.passable_t = True
+        self.passable_t = 1
 
 
     def initTilePos(self) -> None:
 
-        if (self.passable_t == False):
+        if self.passable_t == 0:
             for x in self.tiles:
                 x.passable = False
 
