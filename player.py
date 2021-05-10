3893ffc801ae3b7c7ac35e92854762828a2f63bb
Norbert Kupeczki <norbert2.kupeczki@live.uwe.ac.uk>
2021-05-10 15:51:33 +0100

    Collision Detection complete

--- player.py
+++ player.py
@@ -4,7 +4,8 @@ from projetiles import Projectiles
 
 
 class Player:
-    def __init__(self, start_pos: pyasge.Point2D):
+    def __init__(self, data, start_pos: pyasge.Point2D):
+        self.data = data
         self.sprite = pyasge.Sprite()
         # self.screen_size = screen_size
         self.sprite.loadTexture("/data/images/man_spritesheet.png")
@@ -13,7 +14,7 @@ class Player:
         self.sprite.y = start_pos.y
         self.player_speed = 300
         self.velocity = pyasge.Point2D()
-        self.game_pad_enabled = False  # <-- Change this to true to switch to game pad controls instead of keyboard
+        self.game_pad_enabled = False
         self.facing = pyasge.Point2D(0, 1)
         self.projectiles = Projectiles()
 
@@ -53,10 +54,10 @@ class Player:
         delta_x = self.player_speed * self.velocity.x * game_time.fixed_timestep
         delta_y = self.player_speed * self.velocity.y * game_time.fixed_timestep
 
-        # delta_xy = self.check_collision(delta_x, delta_y)
-        #
-        self.sprite.x = self.sprite.x + delta_x
-        self.sprite.y = self.sprite.y + delta_y
+        delta_xy = self.check_collision(delta_x, delta_y)
+
+        self.sprite.x = self.sprite.x + delta_xy.x
+        self.sprite.y = self.sprite.y + delta_xy.y
 
     def set_sprite(self, x_start: int, width: int):
         self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.START_X] = int(x_start)
@@ -64,24 +65,24 @@ class Player:
         self.sprite.width = int(width)
 
     def get_sprite(self) -> pyasge.Point2D:
-        return pyasge.Point2D(self.sprite.x + self.sprite.width * 0.5, self.sprite.y + self.sprite.height * 0.5)
+        sprite_centre = pyasge.Point2D(self.sprite.x + self.sprite.width * 0.5, self.sprite.y + self.sprite.height * 0.5)
+        self.data.world_loc = sprite_centre
+        self.data.tile_loc = pyasge.Point2D(int(sprite_centre.x / self.data.tile_size), int(sprite_centre.y / self.data.tile_size))
+        return sprite_centre
 
-    # def check_collision(self, dx: float, dy: float) -> Tuple[float, float]:  # <--- Checks collision with the edge of the screen
-    #     if self.sprite.x + dx > (self.screen_size[0] - self.sprite.width):
-    #         self.sprite.x = self.screen_size[0] - self.sprite.width
-    #         dx = 0
-    #     elif self.sprite.x + dx < 0:
-    #         self.sprite.x = 0
-    #         dx = 0
-    #
-    #     if self.sprite.y + dy > (self.screen_size[1] - self.sprite.height):
-    #         self.sprite.y = self.screen_size[1] - self.sprite.height
-    #         dy = 0
-    #     elif self.sprite.y + dy < 0:
-    #         self.sprite.y = 0
-    #         dy = 0
-    #
-    #     return dx, dy
+    def check_collision(self, dx: float, dy: float) -> pyasge.Point2D():
+        bounds = [self.sprite.getWorldBounds().v1,
+                  self.sprite.getWorldBounds().v2,
+                  self.sprite.getWorldBounds().v3,
+                  self.sprite.getWorldBounds().v4]
+
+        for x in bounds:
+            if not self.is_passable(pyasge.Point2D(x.x + dx, x.y)):
+                dx = 0
+            if not self.is_passable(pyasge.Point2D(x.x, x.y + dy)):
+                dy = 0
+
+        return pyasge.Point2D(dx, dy)
 
     def toggle_game_pad(self) -> bool:
         self.game_pad_enabled = not self.game_pad_enabled
@@ -95,3 +96,11 @@ class Player:
         for bullets in self.projectiles.projectiles:
             renderer.render(bullets.sprite)
 
+    def is_passable(self, world_location: pyasge.Point2D()) -> bool:
+        tile_loc = pyasge.Point2D(int(world_location.x / self.data.tile_size),
+                                  int(world_location.y / self.data.tile_size))
+
+        if self.data.map.cost_map[int(tile_loc.y)][int(tile_loc.x)] >= 10000:
+            return False
+        else:
+            return True
