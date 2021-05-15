218d360a68471e055967aeacc10b4ccd0c222dcf
Joseph Khan <joseph2.khan@live.uwe.ac.uk>
2021-05-15 19:04:12 +0100

    Create medkit class

--- medkit.py
+++ medkit.py
@@ -1,15 +1,13 @@
 import pyasge
 
-
-class Gem:
+class Medkit:
     def __init__(self, spawn: pyasge.Point2D):
-        self.value = 15
+        self.id = 2
         self.sprite = pyasge.Sprite()
         self.sprite.loadTexture("/data/images/jewel.png")
         self.sprite.x = spawn.x - self.sprite.width * 0.5
         self.sprite.y = spawn.y - self.sprite.height * 0.5
         self.spawnGem = True
-        self.id = 1
 
     def check_collision(self, player: pyasge.Sprite()):
         if (player.x + player.width > self.sprite.x) and (player.x < self.sprite.x + self.sprite.width):
