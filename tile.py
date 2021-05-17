aff1822c53404bac8ed09608ccd1f56cf297edf7
Norbert Kupeczki <norbert2.kupeczki@live.uwe.ac.uk>
2021-05-17 10:34:15 +0100

    Trying to fix strange numpy array error

--- tile.py
+++ tile.py
@@ -12,14 +12,17 @@ class MapTile:
         self.sprite.setMagFilter(pyasge.MagFilter.NEAREST)
         text_x -= 1
 
-        temp_string_x = str(text_x / 27)
-        temp_string_x = int(temp_string_x.split(".")[0])
-        text_x = text_x - (27 * temp_string_x)
+        # temp_string_x = str(text_x / 27)
+        # temp_string_x = int(temp_string_x.split(".")[0])
+        # text_x = int(text_x - (27 * temp_string_x))
 
-        self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.START_X] = text_x * 64
+        temp_y = int(text_x / 27)
+        text_x = int(text_x - (27 * temp_y))
+
+        self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.START_X] = int(text_x * 64)
         self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.LENGTH_X] = 64
 
-        self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.START_Y] = temp_string_x * 64
+        self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.START_Y] = int(temp_y * 64)
         self.sprite.src_rect[pyasge.Sprite.SourceRectIndex.LENGTH_Y] = 64
 
         self.sprite.width = 64
